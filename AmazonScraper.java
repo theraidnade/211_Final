698cdd51efc781f7bf5e1c4dbdac43677fe03d8c
theraidnade <58786787+theraidnade@users.noreply.github.com>
2022-04-29 19:33:45 -0400

    Update AmazonScraper.java

--- AmazonScraper.java
+++ AmazonScraper.java
@@ -15,37 +15,27 @@ public class AmazonScraper {
 		System.out.println("Please provide the name of the product that you are searching for.");
 		String searchKeyword = userInput.nextLine();
 		searchKeyword = searchKeyword.replace(' ', '+');
-		String productURL = String.format("https://www.amazon.com/s?k=%s&s=price-asc-rank&qid=1649630863&ref=sr_st_price-asc-rank", searchKeyword);
-		try {
-			Document amazonData = Jsoup.connect(productURL).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36").referrer("http://www.google.com").timeout(20000000).followRedirects(true).get();
-			Elements productPrices = amazonData.select(".a-row.a-size-base.a-color-base");
-			for (Element listingPrice : productPrices) {
-				if (listingPrice.select(".a-offscreen").text().length() > 8 || listingPrice.select(".a-offscreen").text().equals("") ) {
-					continue;
+		String productURL = String.format("https://www.amazon.com/s?k=%s&s=price-asc-rank&page=", searchKeyword);
+		for (int urlPage = 0; urlPage < 10; urlPage++) {
+			try {
+				Document amazonData = Jsoup.connect(productURL.concat(String.valueOf(urlPage))).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36").referrer("http://www.google.com").timeout(20000000).followRedirects(true).get();
+				Elements productPrices = amazonData.select(".sg-col-inner");
+				for (Element listingPrice : productPrices) {
+					if (!(listingPrice.select(".a-size-medium.a-color-base.a-text-normal").text().equals(""))) {
+						nameList.add(listingPrice.select(".a-text-normal").text());
+						priceList.add(listingPrice.select(".a-offscreen").text());
+					}
 				}
-				else {
-					String productPrice = listingPrice.select(".a-offscreen").text();
-					priceList.add(productPrice);
+				for (int index = 0; index < priceList.size(); index++) {
+					System.out.println(nameList.get(index) + ": " + priceList.get(index));
 				}
+				System.out.println("////////////////////////");
+				System.out.println(priceList.size());
 			}
-			Elements productNames = amazonData.select(".a-section.a-spacing-none.s-padding-right-small.s-title-instructions-style");
-			for (Element listingName : productNames) {
-				if (listingName.select(".a-size-medium.a-color-base.a-text-normal").text().equals("")) {
-					continue;
-				}
-				else {
-					String productName = listingName.select(".a-size-medium.a-color-base.a-text-normal").text();
-					nameList.add(productName);
-				}
+			catch (Exception scrapeFail) {
+				scrapeFail.printStackTrace();
 			}
-			for (int index = 0; index < priceList.size(); index++) {
-				System.out.println(nameList.get(index) + ": " + priceList.get(index));
-			}
-			System.out.println("////////////////////////");
-		}
-		catch (Exception scrapeFail) {
-			scrapeFail.printStackTrace();
-		}
+	}
 	}
 
 }
