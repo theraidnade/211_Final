c78db493966220d2d283f3af2b01ad3aa9f1155b
theraidnade <58786787+theraidnade@users.noreply.github.com>
2022-04-30 20:42:17 -0400

    Update WalmartScraper.java

--- WalmartScraper.java
+++ WalmartScraper.java
@@ -1,5 +1,6 @@
 package scraperPackage;
 import java.util.ArrayList;
+import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,41 +8,90 @@ import org.jsoup.select.Elements;
 
 public class WalmartScraper {
 
-	public static void main(String[] args) {
-		ArrayList<String> priceList = new ArrayList<String>();
-		ArrayList<String> nameList = new ArrayList<String>();
-		ArrayList<String> linkList = new ArrayList<String>();
-		String searchKeyword = "shampoo";
-		searchKeyword = searchKeyword.replace(' ', '+');
-		String productURL = String.format("https://www.walmart.com/search?q=%s&sort=price_low&page=", searchKeyword);
-		for (int urlPage = 1; urlPage < 11; urlPage++) {
+	public static List<String> getItems(String keyword, int minPrice, int maxPrice){
+		ArrayList<String> productList = new ArrayList<String>();
+		keyword = keyword.replace(' ', '+');
+		String productURL = "";
+		if (minPrice == 0 && maxPrice == 0) {
+			productURL = String.format("https://www.walmart.com/search?q=%s&sort=price_low&page=", keyword);
+		}
+		else if (minPrice != 0 && maxPrice == 0) {
+			productURL = String.format("https://www.walmart.com/search?q=%s&sort=price_low&min_price=%s&page=", keyword, String.valueOf(minPrice));
+		}
+		else if (minPrice == 0 && maxPrice != 0) {
+			productURL = String.format("https://www.walmart.com/search?q=%s&sort=price_low&max_price=%s&page=", keyword, String.valueOf(maxPrice));
+		}
+		else {
+			productURL = String.format("https://www.walmart.com/search?q=%s&sort=price_low&min_price=%s&max_price=%s&page=", keyword, String.valueOf(minPrice), String.valueOf(maxPrice));
+		}
+		for (int urlPage = 1; urlPage < 10; urlPage++) {
 			try {
 				Document walmartData = Jsoup.connect(productURL.concat(String.valueOf(urlPage))).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36").referrer("http://www.google.com").timeout(20000000).followRedirects(true).get();
-				Elements productPrices = walmartData.select(".mb1.ph1.pa0-xl.bb.b--near-white.w-25");
-				for (Element listingPrice : productPrices) {
-					if (!(listingPrice.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text().equals(""))) {
-						if (listingPrice.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text().length() < 300 && listingPrice.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().length() < 10 && listingPrice.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().length() > 0 && (!(nameList.contains(listingPrice.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text())))) {
-							nameList.add(listingPrice.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text());
-							priceList.add(listingPrice.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text());
-							linkList.add(listingPrice.select(".absolute.w-100.h-100.z-1").attr("abs:href"));
+				Elements productsInfo = walmartData.select(".mb1.ph1.pa0-xl.bb.b--near-white.w-25");
+				for (Element listingInfo : productsInfo) {
+					if (!(listingInfo.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text().equals(""))) {
+						if (listingInfo.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text().length() < 300 && listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().length() < 10 && listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().length() > 0 && (!(productList.contains(listingInfo.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text())))) {
+							if (listingInfo.select(".f7.f6-l.gray").text().contains("shipping")) {
+								double basePrice = 0;
+								if (listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().length() <= 7) {
+									basePrice = Double.parseDouble(listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().replace("$", ""));
+								}
+								else {
+									basePrice = Double.parseDouble(listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().replace("$", "").replace(",", ""));
+								}
+								double shippingCost = 0;
+								if (listingInfo.select(".f7.f6-l.gray:contains(shipping)").text().split(" ")[0].length() <= 7) {
+									shippingCost = Double.parseDouble(listingInfo.select(".f7.f6-l.gray:contains(shipping)").text().split(" ")[0].replace("+$", ""));
+								}
+								else {
+									shippingCost = Double.parseDouble(listingInfo.select(".f7.f6-l.gray:contains(shipping)").text().split(" ")[0].replace("+$", "").replace(",", ""));
+								}
+								double actualPrice = basePrice + shippingCost;
+								if (actualPrice < maxPrice && actualPrice > minPrice) {
+									productList.add(listingInfo.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text());
+									productList.add(String.valueOf(actualPrice));
+									productList.add(listingInfo.select(".absolute.w-100.h-100.z-1").attr("abs:href"));
+								}
+							}
+							else {
+								double basePrice = 0.0;
+								if (listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().length() <= 7) {
+									basePrice = Double.parseDouble(listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().replace("$", ""));
+									if (basePrice < maxPrice && basePrice > minPrice) {
+										productList.add(listingInfo.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text());
+										productList.add(String.valueOf(basePrice));
+										productList.add(listingInfo.select(".absolute.w-100.h-100.z-1").attr("abs:href"));
+									}
+								}
+								else {
+									basePrice = Double.parseDouble(listingInfo.select(".b.black.f5.mr1.mr2-xl.lh-copy.f4-l").text().replace("$", "").replace(",", ""));
+									if (basePrice < maxPrice && basePrice > minPrice) {
+										productList.add(listingInfo.select(".f6.f5-l.normal.dark-gray.mb0.mt1.lh-title").text());
+										productList.add(String.valueOf(basePrice));
+										productList.add(listingInfo.select(".absolute.w-100.h-100.z-1").attr("abs:href"));
+									}
+								}
+							}
 						}
 					}
 				}
-				for (int index = 0; index < priceList.size(); index++) {
-					System.out.println(nameList.get(index) + ": " + priceList.get(index));
-					System.out.println(linkList.get(index));
+				for (int index = 0; index < productList.size() - 3; index = index + 3) {
+					System.out.println(productList.get(index) + ": " + productList.get(index+1));
+					System.out.println(productList.get(index+2));
 					System.out.println();
 				}
-				System.out.println("////////////////////////");
-				System.out.println(priceList.size());
 			}
 			catch (Exception scrapeFail) {
 				scrapeFail.printStackTrace();
 			}
 		}
-		System.out.println(nameList);
-		System.out.println(priceList);
-		System.out.println(linkList);
+		System.out.println(productList);
+		return productList;
+	}
+
+
+	public static void main(String[] args) {
+		getItems("grand piano", 1000, 4000);
 	}
 
 }
