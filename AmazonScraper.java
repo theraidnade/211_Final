504521147b1b37d62286ff94608d3448a398dfba
theraidnade <58786787+theraidnade@users.noreply.github.com>
2022-05-10 19:18:26 -0400

    Update AmazonScraper.java
    Final Submission Version with Javadoc comments

--- AmazonScraper.java
+++ AmazonScraper.java
@@ -7,8 +7,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ * Class used to scrape data directly from Amazon.com
+ * @author Phillip Dickey
+ * @version 1.00
+ */
 public class AmazonScraper {
 	
+	/**
+	 * Method used to determine the average price of all of the items found in the getItems() method
+	 * @param items - List of String objects that contains items pulled from the getItems() method
+	 * @return double value representing the average price of all items found by the scraper
+	 */
 	public static double getAverage(List<String> items) {
 		if (items.size() == 0 || items == null) {
 			return -1;
@@ -24,6 +34,11 @@ public class AmazonScraper {
 		return Double.parseDouble(avgFormat.format(averageCost));
 	}
 	
+	/**
+	 * Method used to determine the lowest priced item among all of the items in the getItems() method
+	 * @param items - List of String objects that contains items pulled from the getItems() method
+	 * @return String array containing the [name, price, link] of the lowest priced item
+	 */
 	public static String[] getLowest(List<String> items) {
 		if (items.size() == 0 || items == null) {
 			return null;
@@ -40,28 +55,45 @@ public class AmazonScraper {
 		return returnArray;
 	}
 	
+	/**
+	 * Method used to grab products from Amazon
+	 * @param keyword - String value representing keywords that are passed to Amazon as search terms
+	 * @param minPrice - int value representing the minimum price that a product can be
+	 * @param maxPrice - int value representing the maximum price that a product can be
+	 * @return List<String> - List of string objects that represent the <...,name, price, link, ...> of the products found by the scraper
+	 */
 	public static List<String> getItems(String keyword, int minPrice, int maxPrice){
+		//List of String objects that will be filled with product information and then returned
 		ArrayList<String> productList = new ArrayList<String>();
+		//Replaces any spaces in the parameter with pluses to make it URL friendly
 		keyword = keyword.replace(' ', '+');
 		String productURL = "";
+		//These conditionals determine whether the url needs to include min and max price parameters on Amazon
 		if (minPrice == 0 && maxPrice == 0) {
 			productURL = String.format("https://www.amazon.com/s?k=%s&page=", keyword);
+			maxPrice = Integer.MAX_VALUE;
 		}
 		else if (minPrice != 0 && maxPrice == 0) {
 			productURL = String.format("https://www.amazon.com/s?k=%s&low-price=%s&page=", keyword, String.valueOf(minPrice));
+			maxPrice = Integer.MAX_VALUE;
 		}
 		else if (minPrice == 0 && maxPrice != 0) {
 			productURL = String.format("https://www.amazon.com/s?k=%s&high-price=%s&page=", keyword, String.valueOf(maxPrice));
 		}
 		else {
 			productURL = String.format("https://www.amazon.com/s?k=%s&low-price=%s&high-price=%s&page=", keyword, String.valueOf(minPrice), String.valueOf(maxPrice));
-		}		for (int urlPage = 1; urlPage < 11; urlPage++) {
+		}
+		//This for loop increments through the first 5 pages of results
+		for (int urlPage = 1; urlPage < 7; urlPage++) {
+			//Try catch in the event that Jsoup throws an exception when connecting to Amazon
 			try {
-				Document amazonData = Jsoup.connect(productURL.concat(String.valueOf(urlPage))).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.38 Safari/537.36").ignoreHttpErrors(true).referrer("http://www.google.com").timeout(20000000).followRedirects(true).get();
+				Document amazonData = Jsoup.connect(productURL.concat(String.valueOf(urlPage))).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)").ignoreHttpErrors(true).referrer("http://www.google.com").timeout(20000000).followRedirects(true).get();
 				Elements productsInfo = amazonData.select(".sg-col-inner");
 				for (Element listingInfo : productsInfo) {
+					//Conditionals to check that the Element contains usable data
 					if (!(listingInfo.select(".a-size-medium.a-color-base.a-text-normal").text().equals(""))) {
 						if (listingInfo.select(".a-size-medium.a-color-base.a-text-normal").text().length() < 300 && listingInfo.select(".a-offscreen").text().length() < 10 && listingInfo.select(".a-offscreen").text().length() > 0 && (!(productList.contains(listingInfo.select(".a-size-medium.a-color-base.a-text-normal").text())))) {
+							//Conditional to determine whether shipping cost needs to be calculated and added or not
 							if (listingInfo.select(".a-color-base").text().contains("FREE")) {
 								double basePrice = 0.0;
 								if (listingInfo.select(".a-offscreen").text().length() <= 7) {
@@ -94,7 +126,9 @@ public class AmazonScraper {
 									shippingCost = Double.parseDouble(listingInfo.select("span .a-color-base:contains($)").text().split(" ")[0].replace("$", "").replace(",",""));
 								}
 								double actualPrice = basePrice + shippingCost;
+								//Conditional to ensure final price is within user-provided parameters
 								if (actualPrice < maxPrice && actualPrice > minPrice) {
+									//Adds data in the order <name, price, link>
 									productList.add(listingInfo.select(".a-size-medium.a-color-base.a-text-normal").text());
 									productList.add(String.valueOf(actualPrice));
 									productList.add(listingInfo.select(".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal").attr("abs:href"));
@@ -110,15 +144,16 @@ public class AmazonScraper {
 		}
 		return productList;
 	}
-
-	public static void main(String[] args) {
-		List<String> tempList = getItems("cheese", 2, 2000);
-		System.out.println(tempList);
-		System.out.println(getAverage(tempList));
-		String[] tempStrings = getLowest(tempList);
-		for (String string: tempStrings) {
-			System.out.println(string);
-		}
-	}
+	
+//	Main method used for personal testing
+//	public static void main(String[] args) {
+//		List<String> tempList = getItems("cheese", 2, 2000);
+//		System.out.println(tempList);
+//		System.out.println(getAverage(tempList));
+//		String[] tempStrings = getLowest(tempList);
+//		for (String string: tempStrings) {
+//			System.out.println(string);
+//		}
+//	}
 
 }
