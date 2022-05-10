902ca61f150c9bcde4c6607684963956b4cbb1c4
theraidnade <58786787+theraidnade@users.noreply.github.com>
2022-05-10 19:38:15 -0400

    Addition of Documentation v2
    additional comments added

--- AmazonScraper.java
+++ AmazonScraper.java
@@ -23,10 +23,13 @@ public class AmazonScraper {
 		if (items.size() == 0 || items == null) {
 			return -1;
 		}
+		//DecimalFormat used to ensure numerical cleanliness in GUI environment
 		DecimalFormat avgFormat = new DecimalFormat("#.00");
+		//Gets size of list and divides by three due to items having three values associated with them
 		int numItems = items.size()/3;
 		double tempPrice = 0.0;
 		double averageCost = 0.0;
+		//Adds all prices into the tempPrice variable
 		for (int i = 0; i < items.size(); i = i+3) {
 			tempPrice += Double.parseDouble(items.get(i+1));
 		}
@@ -45,18 +48,20 @@ public class AmazonScraper {
 		}
 		double lowVal = Double.MAX_VALUE;
 		int minIndex = 0;
+		//Checks every item and its price for being the lowest
 		for (int i = 0; i < items.size(); i = i+3) {
 			if (Double.parseDouble(items.get(i+1)) < lowVal) {
 				minIndex = i;
 				lowVal = Double.parseDouble(items.get(i+1));
 			}
 		}
+		//Adds [Name, price, link] to String array
 		String[] returnArray = {items.get(minIndex), items.get(minIndex+1), items.get(minIndex+2)};
 		return returnArray;
 	}
 	
 	/**
-	 * Method used to grab products from Amazon
+	 * Method used to grab products from Amazon using the jsoup api
 	 * @param keyword - String value representing keywords that are passed to Amazon as search terms
 	 * @param minPrice - int value representing the minimum price that a product can be
 	 * @param maxPrice - int value representing the maximum price that a product can be
@@ -69,6 +74,8 @@ public class AmazonScraper {
 		keyword = keyword.replace(' ', '+');
 		String productURL = "";
 		//These conditionals determine whether the url needs to include min and max price parameters on Amazon
+		//Zero is the default for both minPrice and maxPrice so if it is detected it will use a URL with no min or max
+		//It will then set maxPrice equal to the max Integer value so it does not cause problems later on
 		if (minPrice == 0 && maxPrice == 0) {
 			productURL = String.format("https://www.amazon.com/s?k=%s&page=", keyword);
 			maxPrice = Integer.MAX_VALUE;
@@ -83,12 +90,15 @@ public class AmazonScraper {
 		else {
 			productURL = String.format("https://www.amazon.com/s?k=%s&low-price=%s&high-price=%s&page=", keyword, String.valueOf(minPrice), String.valueOf(maxPrice));
 		}
-		//This for loop increments through the first 5 pages of results
-		for (int urlPage = 1; urlPage < 7; urlPage++) {
+		//This for loop increments through the first 10 pages of results
+		for (int urlPage = 1; urlPage < 10; urlPage++) {
 			//Try catch in the event that Jsoup throws an exception when connecting to Amazon
 			try {
+				//HTML Document object of data found on the specified walmart page
 				Document amazonData = Jsoup.connect(productURL.concat(String.valueOf(urlPage))).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)").ignoreHttpErrors(true).referrer("http://www.google.com").timeout(20000000).followRedirects(true).get();
+				//Elements object of the product panels on the Document
 				Elements productsInfo = amazonData.select(".sg-col-inner");
+				//Enhanced for loop used to grab individual products and then manipulate their data
 				for (Element listingInfo : productsInfo) {
 					//Conditionals to check that the Element contains usable data
 					if (!(listingInfo.select(".a-size-medium.a-color-base.a-text-normal").text().equals(""))) {
@@ -102,7 +112,9 @@ public class AmazonScraper {
 								else {
 									basePrice = Double.parseDouble(listingInfo.select(".a-offscreen").text().replace("$", "").replace(",", ""));
 								}
+								//Conditional to ensure final price is within user-provided parameters
 								if (basePrice < maxPrice && basePrice > minPrice) {
+									//Adds data in the order <name, price, link>
 									productList.add(listingInfo.select(".a-size-medium.a-color-base.a-text-normal").text());
 									productList.add(String.valueOf(basePrice));
 									productList.add(listingInfo.select(".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal").attr("abs:href"));
@@ -145,7 +157,7 @@ public class AmazonScraper {
 		return productList;
 	}
 	
-//	Main method used for personal testing.
+//	Main method used for personal testing
 //	public static void main(String[] args) {
 //		List<String> tempList = getItems("cheese", 2, 2000);
 //		System.out.println(tempList);
